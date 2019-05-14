01c763ff9fb3e025fa17268a1bc1c6ee5f24402c
jinholy <wowwowjinho@gmail.com>
2019-05-14 12:05:01 +0900

    comment added

--- download.cpp
+++ download.cpp
@@ -3,21 +3,24 @@
 #include <string>
 
 using namespace std;
-extern "C" void download(const char* url){
-    ifstream in("extList.txt", ios::in | ios::binary);
-    if (!in)
+//cpp함수를　c에서　사용하기위해　extern "C"　를　함수앞에　붙여준다．
+extern "C" void download(const char* url){              
+    //url을　전달받아서　단순히 wget url filename　이런식으로　만들어서　wget　명령어를　system　함수를　통해　실행하는　함수
+    ifstream in("extList.txt", ios::in | ios::binary);              //원하는　ext만　뽑아　낸　extList.txt를　읽어들인다．
+    if (!in)        //에러처리．
     {
         cout << "File " << "extList.txt" << " not found!\n";
         exit(EXIT_FAILURE);
     }
 
     string line;
-    while (getline(in, line))
+    while (getline(in, line))       //파일에서　한줄씩　읽어들인다．
     {
-        string c_url = url;
-        string tmp = "wget "+ c_url +line;
-        const char *cmd = tmp.c_str();
-        system(cmd);
+        //c++에서　string과　const char*를　이어　붙이기　위한　험난한　여정
+        string c_url = url;                 
+        string tmp = "wget "+ c_url +line;          //wget + 실행할　때　파라메터로　넘겨준　url주소 + extList에서　읽어들인　파일이름
+        const char *cmd = tmp.c_str();              //string을　c_str()로　const char*로　바꿔줌．
+        system(cmd);                                //그렇게　완성된　wget 주소＋파일명　으로　wget명령어　수행．
     }
 
     in.close();
--- makelistwithext.cpp
+++ makelistwithext.cpp
@@ -3,9 +3,10 @@
 #include <string>
 
 using namespace std;
+//C++ 함수를　c에서　사용하려면　함수　앞에　extern "C"를　붙여주면된다．　
 extern "C" void makeListwithExt(char* ext){
-    ifstream in("hrefList.txt", ios::in | ios::binary);
-    ofstream out("extList.txt", ios::out | ofstream::binary);
+    ifstream in("hrefList.txt", ios::in | ios::binary);             //href　태그가　붙은거를　모아둔　txt파일을　읽음
+    ofstream out("extList.txt", ios::out | ofstream::binary);       //원하는　확장자만　모아둔　txt에　쓸것．
 
     if (!in)
     {
@@ -17,9 +18,11 @@ extern "C" void makeListwithExt(char* ext){
     while (getline(in, line))
     {
         size_t exists = line.find(ext);
-        if (exists!=string::npos)
+        if (exists!=string::npos)           
+        //문자열　안에　ext (예를들면　.zip)이　존재하는지　확인．　find("str")이
+        //원래　어디에　있는지　위치를　알려주는　함수인데　단순히　bool로　정의해서　쓰면　무조건　true나와서　이런식으로　사용
         { //simplely add to extList.txt if ext inlucded
-            out << line << endl;
+            out << line << endl;                //원하는　ext가　문자열에　있으면　파일에　써준다．
         }
     }
 
